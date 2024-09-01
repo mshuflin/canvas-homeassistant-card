25ee575ec6d451a2f2f8960f4be1ce0f46a4f8eb
mshuflin <53985056+mshuflin@users.noreply.github.com>
2024-09-01 15:10:08 +0800

    Update custom-canvas-homework-card.js
    changed reactive element link

--- custom-canvas-homework-card.js
+++ custom-canvas-homework-card.js
@@ -1,7 +1,7 @@
 import { LitElement, html } from "https://cdn.jsdelivr.net/npm/lit-element@4.1.0/lit-element.min.js";
 import { classMap } from "https://cdn.jsdelivr.net/npm/lit-html@3.2.0/lit-html.min.js";
 import { unsafeHTML } from "https://cdn.jsdelivr.net/npm/dangerous-html@0.1.13/dist/react/lib.min.js";
-import { ReactiveElement } from '@lit/reactive-element';
+import { ReactiveElement } from "https://cdn.jsdelivr.net/npm/@lit/reactive-element@2.0.4/css-tag.min.js";
 
 // Configure the preview in the Lovelace card picker
 window.customCards = window.customCards || [];
